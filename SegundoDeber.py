284c42ff0a18d5aff0f546c185985c7972e1f6ec
DianaCaSaB <114881485+DianaCaSaB@users.noreply.github.com>
2023-07-31 18:04:31 -0500

    Add files via upload

---
+++ SegundoDeber.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 31 17:58:52 2023
+
+@author: pc
+"""
+
+str1="Cisco"
+str2="Networking"
+str3="Academy"
+space=" "
+print(str1+space+str2+space+str3)
