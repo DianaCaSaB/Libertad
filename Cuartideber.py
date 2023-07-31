463502763599f478a873853b642906a57d80eb89
DianaCaSaB <114881485+DianaCaSaB@users.noreply.github.com>
2023-07-31 18:15:50 -0500

    Add files via upload

---
+++ Cuartideber.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+pi=22/7
+print(pi)
+print("{:.2f}".format(pi))
+print("{:.6f}".format(pi))
+print("{:.50f}".format(pi))
+print("{:.100f}".format(pi))
+
+
+
+
+
+     
+
+
